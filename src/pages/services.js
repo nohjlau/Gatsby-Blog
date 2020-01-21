5a14caf69cb1a023a90e8bb5092ec3133ee09ecc
John Lau <nohjlau@gmail.com>
2020-01-21 14:48:02 -0800

    Implemented site navigation

---
+++ src/pages/services.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ServicesPage = () => {
+  return(
+  <Layout>
+    <div>
+      <h1>Our services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sit
+        eveniet iure accusantium dolore est reprehenderit? Quo at quae aliquid
+        amet quam. Itaque, dicta esse saepe molestiae obcaecati ipsum quas
+        suscipit libero voluptatum, corrupti, reiciendis perferendis dolorem
+        sapiente non consectetur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sit
+        eveniet iure accusantium dolore est reprehenderit? Quo at quae aliquid
+        amet quam. Itaque, dicta esse saepe molestiae obcaecati ipsum quas
+        suscipit libero voluptatum, corrupti, reiciendis perferendis dolorem
+        sapiente non consectetur.
+      </p>
+    </div>
+  </Layout>
+  )
+}
+
+export default ServicesPage
