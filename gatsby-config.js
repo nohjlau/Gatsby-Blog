5912213934fae2bfe5ba0b0d9bb5f2d11df02754
John Lau <nohjlau@gmail.com>
2020-01-21 14:33:22 -0800

    Updated config and added about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `A simple blog following Traversy Media's Gatsby Crash Course Tutorial.`,
+    author: `@nohjlau`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return(
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sit
+        eveniet iure accusantium dolore est reprehenderit? Quo at quae aliquid
+        amet quam. Itaque, dicta esse saepe molestiae obcaecati ipsum quas
+        suscipit libero voluptatum, corrupti, reiciendis perferendis dolorem
+        sapiente non consectetur.
+      </p>
+    </div>
+  </Layout>
+  )
+}
+
+export default AboutPage
